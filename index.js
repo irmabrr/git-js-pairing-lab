2a4fb4a50bd64c32b428ce79f644621cfbee5114
Irma <barrios12214@gmail.com>
2024-10-02 14:24:35 -0400

    ?

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-//Code your solutions in this file
-let fiveToOneHundred = () =>{
+ //Code your solutions in this file
+ let fiveToOneHundred = () =>{
 
 for (i=5; i <= 100; i++){
-    console.log (i);
-}
-}
+     console.log (i);
+    }
+ }
 
 fiveToOneHundred();
+
