6265e9dfe705aa34dcdddeaf96a8ebd2d51d1b2c
Irma <barrios12214@gmail.com>
2024-10-02 15:43:00 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-//Question 1
-let fiveToOneHundred = () =>{
+// //Question 1
+ let fiveToOneHundred = () =>{
 
-    for (i=5; i <= 100; i++){
-        console.log (i);
+     for (i=5; i <= 100; i++){
+         console.log (i);
+     }
+ }
+
+ console.log(fiveToOneHundred());
+
+ //Question 2
+ const multiplesOfThree = () => {
+     for (i=3; i <=  100; i = i+3){
+         console.log(i);
+     }
+ }
+ console.log(multiplesOfThree());
+
+ // question 3
+ let multiplesOfThreeOrFive = () =>{
+     for (i=3; i <=  100; i++){  
+         if (i % 3 === 0 || i % 5 === 0){
+             console.log (i);
+         } 
     }
-}
+ }
 
-console.log(fiveToOneHundred());
 
-//Question 2
-const multiplesOfThree = () => {
-    for (i=3; i <=  100; i = i+3){
+ console.log(multiplesOfThreeOrFive())
+
+ //Question 4
+ const untilNum = num => {
+         for (i=1; i<=num; i++){
         console.log(i);
-    }
-}
-console.log(multiplesOfThree());
-
-let multiplesOfThreeOrFive = () =>{
-    for (i=3; i <=  100; i++){  
-        if (i % 3 === 0 || i % 5 === 0){
-            console.log (i);
-        } 
-    }
-}
-
-
-console.log(multiplesOfThreeOrFive())
-
-//Question 4
-const untilNum = num => {
-    for (i=1; i<=num; i++){
-        console.log(i);
-    }
+     }
 }
 
 untilNum(5);
 
+// question 5
 const multiply = (prod1, prod2) => {
    return prod1 * prod2; 
 }
